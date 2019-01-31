047a6799de781855f353a604a0b6ff5eaeb52a3a
npklein <niekdeklein@gmail.com>
2019-01-31 16:11:42 +0100

    binom test

--- ASE_binomial_test/binom_test.R
+++ ASE_binomial_test/binom_test.R
@@ -3,13 +3,13 @@
 library(ggplot2)
 library(reshape2)
 library(dplyr)
-library(plyr)
 library(data.table)
 
+input_and_output_dir <- '/groups/umcg-bios/tmp03/projects/outlierGeneASE/binomialTest/'
 #### READ IN A AND B COUNTS ####
-aCount <- read.table('/groups/umcg-bios/tmp03/projects/outlierGeneASE/binomialTest/genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditing.aCount.txt', header=T, check.names=FALSE)
-bCount <- read.table('/groups/umcg-bios/tmp03/projects/outlierGeneASE/binomialTest/genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditing.bCount.txt',header=T, check.names=FALSE)
-annotations <- as.data.frame(fread("sample_genes_outliers_phenotypes.removedCODAM.txt"))
+aCount <- read.table(paste0(input_and_output_dir,'genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditing.aCount.txt'), header=T, check.names=FALSE)
+bCount <- read.table(paste0(input_and_output_dir,'genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditing.bCount.txt'),header=T, check.names=FALSE)
+annotations <- as.data.frame(fread("/groups/umcg-bios/tmp03/projects/outlierGeneASE/binomialTest/sample_genes_outliers_phenotypes.removedCODAM.txt"))
 
 
 abcount_all_genes <- data.frame()
@@ -45,21 +45,21 @@ for(gene in unique(aCount$ENSEMBLID)){
   ab_count$biobank <- annotations[match(as.character(ab_count$sample), annotations$SAMPLE),]$biobank_id
   #####
   
-  ab_count %>%
-    group_by(ENSEMBLID, biobank) %>%
-    summarise_at(vars(-ratio), funs(mean(., na.rm=TRUE)))
+  #ab_count %>%
+  #  group_by(ENSEMBLID, biobank) %>%
+  #  summarise_at(vars(-ratio), funs(mean(., na.rm=TRUE)))
   
   # calcualte the mean ratio per biobank to use as new p
-  av_ratio_per_biobank <- ddply(ab_count, .(ENSEMBLID, biobank), summarize,  ratio=mean(ratio))
+#  av_ratio_per_biobank <- ddply(ab_count, .(ENSEMBLID, biobank), summarize,  ratio=mean(ratio))
   
-  ab_count$binom_pvals_other_p <- apply(ab_count, 1, function(x) {
-    av_gene  <- av_ratio_per_biobank[x['ENSEMBLID']==av_ratio_per_biobank$ENSEMBLID, ] 
-    biobank_p <- av_gene[match(x['biobank'], av_gene$biobank),]$ratio
+#  ab_count$binom_pvals_other_p <- apply(ab_count, 1, function(x) {
+#    av_gene  <- av_ratio_per_biobank[x['ENSEMBLID']==av_ratio_per_biobank$ENSEMBLID, ] 
+#    biobank_p <- av_gene[match(x['biobank'], av_gene$biobank),]$ratio
                                          
-    binom.test(as.numeric(x[c('aCount','bCount')]), alternative="two.sided", 
-                 p=biobank_p, 
-                 conf.level=0.95)$p.value
-    })
+#    binom.test(as.numeric(x[c('aCount','bCount')]), alternative="two.sided", 
+#                 p=biobank_p, 
+#                 conf.level=0.95)$p.value
+#    })
   
   ab_count$binom_pvals <- apply(ab_count, 1, function(x) {
     binom.test(as.numeric(x[c('aCount','bCount')]), alternative="two.sided", 
@@ -70,10 +70,10 @@ for(gene in unique(aCount$ENSEMBLID)){
   ab_count$p_bonferroni_gene <- p.adjust(ab_count$binom_pvals, method='bonferroni')
   ab_count$p_fdr_gene <- p.adjust(ab_count$binom_pvals, method='fdr')
   abcount_all_genes <- rbind(abcount_all_genes, ab_count)
-  write.table(ab_count, paste0('genes/',gene,'.txt'))
+  write.table(ab_count, paste0(input_and_output_dir,'genes/',gene,'.txt'))
 }  
 
 abcount_all_genes$p_bonferroni <- p.adjust(abcount_all_genes$binom_pvals, method='bonferroni')
 abcount_all_genes$p_fdr <- p.adjust(abcount_all_genes$binom_pvals, method='fdr')
-write.table(abcount_all_genes, paste0('binom_ASE_all_genes.txt'), quote = F, sep='\t')
+write.table(abcount_all_genes, paste0(input_and_output_dir, 'binom_ASE_all_genes.txt'), quote = F, sep='\t')
 
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # BIOS_ASE
 
-
 # Generate count files for haplotype A and B from geneAE data
 sh createCountTables.sh
 
 # Perform binominal test on count data
+
+# Run binomial test on the a/b counts
+Rscript ASE_binomial_test/binom_test.R
+
+# Make table with ASE genes (bonf. corrected p-value < 0.05
+python ASE_outlier_table/make_outlier_table.py
