22673b1f49cb440c593494d29dcb716a583ec78b
npklein <niekdeklein@gmail.com>
2019-02-18 11:55:43 +0100

    calculate binom pval snp aggregate

--- ASE_binomial_test/binom_snp_aggregate_test.R
+++ ASE_binomial_test/binom_snp_aggregate_test.R
@@ -2,47 +2,61 @@
 
 library(ggplot2)
 library(reshape2)
+library(plyr)
 library(dplyr)
 library(data.table)
 library(bbmle)
 
 #### Read in file with the ref/alt counts ####
+print('Read data')
 ase_counts_snp_aggregate <- fread('/groups/umcg-bios/tmp03/projects/BIOS_manuscript/ase_ase.txt')
 ase_counts_sample <- fread('/groups/umcg-bios/tmp03/projects/BIOS_manuscript/ase_sampleAse.txt')
 ase_counts_sample$Alt_Counts <- as.numeric(ase_counts_sample$Alt_Counts)
 ase_counts_sample$Ref_Counts <- as.numeric(ase_counts_sample$Ref_Counts)
 
 #### First sum the ref and alt per snp ####
+print('Aggregate SNP ref/alt counts')
 allele_counts_per_snp <- ase_counts_sample %>% 
   group_by(snp_id) %>% 
   summarise(Ref_Counts = sum(Ref_Counts),
             Alt_Counts = sum(Alt_Counts))
 allele_counts_per_snp$total <- allele_counts_per_snp$Ref_Counts + allele_counts_per_snp$Alt_Counts
 
-allele_counts_per_snp <- head(allele_counts_per_snp)
 ####
 
 #### Calculate the log ratio ####
-logL_function <- function(p, snp) -sum(dbinom(as.numeric(snp[['Ref_Counts']]),
+testFunction <- function (date_in) {
+  return(tryCatch(as.Date(date_in), error=function(e) NULL))
+}
+
+logL_function <- function(p, snp){
+    dbinom(as.numeric(snp[['Ref_Counts']]),
                                           as.numeric(snp[['total']]),
-                                          p, log=T))
+                                          p, log=T)
+}
 
+#print('run maximium likelihood')
+#logL_sumary <- apply(allele_counts_per_snp, 1, function(x)
+#                            summary(mle2(logL_function, 
+#                                 start=list(p=0.5), 
+#                                 data=list(snp=x)))
 
-logL_sumary <- apply(allele_counts_per_snp, 1, function(x)
-                            summary(mle2(logL_function, 
-                                 start=list(p=0.5), 
-                                 data=list(snp=x))))
 # can't get it nicely into the column in a different way, so apply and unlist
-allele_counts_per_snp$Likelihood_ratio <- unlist(lapply(logL_sumary, function(x) x@m2logL))
-allele_counts_per_snp$Likelihood_ratio_test_Pval <- unlist(lapply(logL_sumary, function(x) x@coef[4]))
+#allele_counts_per_snp$Likelihood_ratio <- unlist(lapply(logL_sumary, function(x) x@m2logL))
+allele_counts_per_snp$Likelihood_ratio <- "NA"
+#allele_counts_per_snp$Likelihood_ratio_test_Pval <- unlist(lapply(logL_sumary, function(x) x@coef[4]))
+allele_counts_per_snp$Likelihood_ratio_test_Pval <- "NA"
 
-allele_counts_per_snp$Likelihood_ratio_test_FDR <- p.adjust(allele_counts_per_snp$Likelihood_ratio_test_Pval, 
-                                                            method='fdr')
-allele_counts_per_snp$Likelihood_ratio_test_bonferroni <- p.adjust(allele_counts_per_snp$Likelihood_ratio_test_Pval, 
-                                                                    method='bonferroni')
+#allele_counts_per_snp$Likelihood_ratio_test_FDR <- p.adjust(allele_counts_per_snp$Likelihood_ratio_test_Pval, 
+#                                                            method='fdr')
+#allele_counts_per_snp$Likelihood_ratio_test_bonferroni <- p.adjust(allele_counts_per_snp$Likelihood_ratio_test_Pval, 
+#                                                                    method='bonferroni')
+allele_counts_per_snp$Likelihood_ratio_test_FDR <- "NA"
+allele_counts_per_snp$Likelihood_ratio_test_bonferroni <- "NA"
 ####
 
 #### Calculate binomial p-value ####
+print('Run binomial test')
 allele_counts_per_snp$binom_Pval <- apply(allele_counts_per_snp, 1, function(x) {
     binom.test(as.numeric(x['Ref_Counts']),
                as.numeric(x['Ref_Counts'])+as.numeric(x['Alt_Counts']), 
@@ -66,6 +80,7 @@ ase_counts_snp_aggregate_binom <- ase_counts_snp_aggregate_binom[c('SNP_ID','Fra
                                                                    'Likelihood_ratio_test_Pval','Likelihood_ratio_test_FDR',
                                                                    'Likelihood_ratio_test_bonferroni','binom_Pval',
                                                                    'binom_FDR','binom_bonferroni')]
+print('write to /groups/umcg-bios/tmp03/projects/BIOS_manuscript/ase_snpAse.binom.txt')
 write.table(ase_counts_snp_aggregate_binom, '/groups/umcg-bios/tmp03/projects/BIOS_manuscript/ase_snpAse.binom.txt',
             sep='\t',
             row.names=F)
