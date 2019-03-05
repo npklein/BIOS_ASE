d351e1a7f3a05434773ae62109a0e75639d7cff1
npklein <niekdeklein@gmail.com>
2019-03-05 12:00:34 +0100

    add for merge

--- figure_4/plot_fraction_minor_alleles_per_impact_category.R
+++ figure_4/plot_fraction_minor_alleles_per_impact_category.R
@@ -1,7 +1,6 @@
 library(ggplot2)
 library(data.table)
 library(reshape2)
-library(ggforce)
 library(ggpubr)
 library(ggsignif)
 
@@ -40,33 +39,47 @@ summed_counts$SNPEFFIMPACT <- factor(summed_counts$SNPEFFIMPACT, levels=c('MODIF
 summed_counts$minorRatio <- summed_counts$minor/(summed_counts$minor+summed_counts$major)
 
 
-summed_counts_filtered<-summed_counts[summed_counts$SNPEFFIMPACT == "LOW" | summed_counts$SNPEFFIMPACT == "MODERATE" | summed_counts$SNPEFFIMPACT == "HIGH",]
+summed_counts_filtered <- summed_counts[summed_counts$SNPEFFIMPACT == "LOW" | summed_counts$SNPEFFIMPACT == "MODERATE" | summed_counts$SNPEFFIMPACT == "HIGH",]
 
-#summed_counts1<-summed_counts[summed_counts$AF <= 0.01,]
-summed_counts1<-summed_counts_filtered
 
-t.test(summed_counts1[summed_counts1$SNPEFFIMPACT=="HIGH",]$minorRatio, summed_counts1[summed_counts1$SNPEFFIMPACT=='MODERATE',]$minorRatio)$p.val
-t.test(summed_counts1[summed_counts1$SNPEFFIMPACT=="HIGH",]$minorRatio, summed_counts1[summed_counts1$SNPEFFIMPACT=='LOW',]$minorRatio)$p.val
-t.test(summed_counts1[summed_counts1$SNPEFFIMPACT=="MODERATE",]$minorRatio, summed_counts1[summed_counts1$SNPEFFIMPACT=='LOW',]$minorRatio)$p.val
-
-comparisons <- list( c("HIGH", "MODERATE"), c("HIGH", "LOW"), c("MODERATE", "LOW") )
+#comparisons <- list( c("HIGH", "MODERATE"), c("HIGH", "LOW"), c("MODERATE", "LOW") )
 give.n <- function(x){
   return(c(y = -0.03, label = length(x)))
 }
 
+summed_counts_filtered$frac_minor_allele <- summed_counts_filtered$minor/(summed_counts_filtered$minor+summed_counts_filtered$major)
 
-ggplot(data=summed_counts1, aes(summed_counts1$SNPEFFIMPACT, summed_counts1$minor/(summed_counts1$minor+summed_counts1$major), fill = summed_counts1$SNPEFFIMPACT))+
+# Calculate the p-values
+comparisons <- data.frame(compare_means(frac_minor_allele ~SNPEFFIMPACT,  
+                             data = summed_counts_filtered,
+                              method = "wilcox.test"))
+
+# make datarfame with same colnames as summed_counts_filtered
+colnames(comparisons)[colnames(comparisons)=='group2'] <- 'SNPEFFIMPACT'
+comparisons$frac_minor_allele <- NA
+comparisons[comparisons$SNPEFFIMPACT == 'HIGH' & comparisons$group1=='MODERATE',]$frac_minor_allele <- 1.1
+comparisons[comparisons$SNPEFFIMPACT == 'HIGH' & comparisons$group1=='LOW',]$frac_minor_allele <- 1.2
+comparisons[comparisons$SNPEFFIMPACT == 'MODERATE' & comparisons$group1=='LOW',]$frac_minor_allele <- 1.3
+
+ 
+p <- ggplot(data=summed_counts_filtered, aes(SNPEFFIMPACT, 
+                                             frac_minor_allele,
+                                             fill = SNPEFFIMPACT))+
   geom_violin()+
   geom_boxplot(width=0.05)+
   theme_bw(base_size = 18)+
   xlab('Functional impact')+
-  ylab('Fraction minor alleles\n(Minor allele counts / total counts)')+
+  ylab('fracion minor alleles\n(Minor allele counts / total counts)')+
   guides(fill=F)+
-  #ggtitle("Rare variants (AF <= 0.01)")+
   scale_fill_brewer(palette="Dark2")+ 
-  stat_compare_means(comparisons = comparisons, method='wilcox.test')+
-  stat_summary(fun.data = give.n, geom = "text")
+  stat_summary(fun.data = give.n, geom = "text")+
+  geom_text(data = comparisons, aes(x = SNPEFFIMPACT, y =frac_minor_allele , label = p.adj), size = 10)
+
+  #ggtitle("Rare variants (AF <= 0.01)")+  
+#  stat_compare_means(aes(label = ..p.adj..),
+#                    comparisons = comparisons, method='wilcox.test')+
+
   
-  
-#ggsave('/Users/freerkvandijk/Downloads/minor_allele_fraction.manuscript.20190303.png',width=8, height=8)
+ggsave('/groups/umcg-bios/tmp03/projects/BIOS_manuscript/fig4/minor_allele_fracion.manuscript.20190303.png',
+        plot=p,  width=8, height=8)
 
