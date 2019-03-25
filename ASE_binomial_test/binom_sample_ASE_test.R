c358a0bf4afb72da7825dff52e80ad6f8751cc58
freerkvandijk <freerk.van.dijk@gmail.com>
2019-03-25 15:21:59 +0100

    Renamed binom test scripts

--- ASE_binomial_test/binom_sample_ASE_test.R
+++ ASE_binomial_test/binom_sample_ASE_test.R
@@ -6,7 +6,7 @@ library(dplyr)
 library(data.table)
 
 #### Read in file with the ref/alt counts ####
-ase_counts <- fread('/groups/umcg-bios/tmp03/projects/BIOS_manuscript/ase_sampleAse.txt')
+ase_counts <- fread('/groups/umcg-bios/tmp03/projects/BIOS_manuscript/ase_sampleAse.all.txt')
 
 
 ase_counts$Pval <- apply(ase_counts, 1, function(x) {
@@ -20,7 +20,16 @@ ase_counts$Pval <- apply(ase_counts, 1, function(x) {
 ase_counts$Bonf_corr_Pval <- p.adjust(ase_counts$Pval, method='bonferroni')
 ase_counts$FDR <- p.adjust(ase_counts$Pval, method='fdr')
 
-write.table(ase_counts, '/groups/umcg-bios/tmp03/projects/BIOS_manuscript/ase_sampleAse.binom.txt',
+ase_counts$Ref_Counts <- paste0("", ase_counts$Ref_Counts, "")
+ase_counts$Alt_Counts <- paste0("", ase_counts$Alt_Counts, "")
+ase_counts$Chromosome <- paste0("", ase_counts$Chromosome, "")
+ase_counts$Position <- paste0("", ase_counts$Position, "")
+ase_counts$ID <- paste0("", ase_counts$ID, "")
+ase_counts$Pval <- paste0("", ase_counts$Pval, "")
+ase_counts$Bonf_corr_Pval <- paste0("", ase_counts$Bonf_corr_Pval, "")
+ase_counts$FDR <- paste0("", ase_counts$FDR, "")
+
+write.table(ase_counts, '/groups/umcg-bios/tmp03/projects/BIOS_manuscript/ase_sampleAse.all.binom.txt',
             sep='\t',
             row.names=F)
 
