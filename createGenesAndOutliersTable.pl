574acffbd5fd7a76666a433098c8958051c5fc2f
npklein <niekdeklein@gmail.com>
2019-04-02 15:09:22 +0200

    create table using FDR table instead of bonf table

--- createGenesAndOutliersTable.pl
+++ createGenesAndOutliersTable.pl
@@ -11,7 +11,7 @@ use Scalar::Util qw(looks_like_number);
 
 
 
-open(BINOM, "< /groups/umcg-bios/tmp03/projects/outlierGeneASE/logFoldChangeTables/genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditing.logFoldChange.depthFiltere.BINOM.Bonferroni.txt") || die "Can't open input BINOM file!\n";
+open(BINOM, "< /groups/umcg-bios/tmp03/projects/outlierGeneASE/logFoldChangeTables/genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditing.logFoldChange.depthFiltere.BINOM.txt") || die "Can't open input BINOM file!\n";
 my @BINOMfile = <BINOM>;
 close(BINOM);
 my $BINOMhead = $BINOMfile[0];
