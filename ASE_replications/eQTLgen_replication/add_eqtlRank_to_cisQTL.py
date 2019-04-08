f3a40845da49c3d552aac7a1289c73f4632ab7f6
npklein <niekdeklein@gmail.com>
2019-04-08 15:28:43 +0200

    add eqtlgen rank, use geneAE table

---
+++ ASE_replications/eQTLgen_replication/add_eqtlRank_to_cisQTL.py
@@ -0,0 +1,21 @@
+
+eqtl_rank = {}
+with open('eqtlRank.txt') as input_file:
+    for line in input_file:
+        line = line.strip().split('\t')
+        if len(line) == 1:
+            continue
+        eqtl_rank[line[0]+'_'+line[1]] = line[2]
+
+f_in = '2018-01-31-cis-eQTLsFDR-ProbeLevel-CohortInfoRemoved.aseSNPs.snpsInProtCodingExon.txt'
+f_out = '2018-01-31-cis-eQTLsFDR-ProbeLevel-CohortInfoRemoved.aseSNPs.snpsInProtCodingExon.eqtlGenRank.txt'
+with open(f_in) as input_file,  open(f_out,'w') as out:
+    header = input_file.readline().strip()+'\teqtlGenRank\n'
+    out.write(header)
+    for line in input_file:
+        line = line.strip()
+        chr = line.split('\t')[3]
+        pos = line.split('\t')[4]
+        snp = chr+'_'+pos
+        eqtlGen_rank = eqtl_rank[line.split('\t')[0]+'_'+snp]
+        out.write(line+'\t'+eqtlGen_rank+'\n')
