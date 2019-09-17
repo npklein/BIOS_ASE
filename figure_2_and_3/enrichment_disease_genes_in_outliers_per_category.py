1041023f62602a672a43c53b9f336dbcc199a031
npklein <niekdeklein@gmail.com>
2019-09-17 13:17:15 +0200

    omim enrichment also includes for non-hets

--- figure_2_and_3/enrichment_disease_genes_in_outliers_per_category.py
+++ figure_2_and_3/enrichment_disease_genes_in_outliers_per_category.py
@@ -6,6 +6,7 @@ manifestations = set([])
 inheritances = set([])
 # TOD: how was this file made
 with open('/groups/umcg-bios/tmp03/projects/outlierGeneASE/geneAndVariantLists/CGD.20171220.txt') as input_file:
+    print('Read CGD file')
     header = input_file.readline().strip().split('\t')
     for line in input_file:
         line = line.strip().split('\t')
@@ -21,9 +22,10 @@ with open('/groups/umcg-bios/tmp03/projects/outlierGeneASE/geneAndVariantLists/C
             manifestations.add(manifestationName)
 
 logFC_per_sample_per_gene = {}
-# TODO: how was this file made 
+# Input file made by ../createLogFoldChangeTable.pl
 input_dir = '/groups/umcg-bios/tmp03/projects/outlierGeneASE//logFoldChangeTables/'
 with open(input_dir+'genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditing.logFoldChange.depthFiltere.BINOM.samplesFILTERED.values.txt') as input_file:
+    print('read logFC file')
     header = input_file.readline().strip().split('\t')
     header_index = {}
     for index, element in enumerate(header):
@@ -39,8 +41,9 @@ with open(input_dir+'genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditi
 outliers = {}
 not_outliers = {}
 na = {}
-# TODO: how was this file made 
+# Input file made by ../createLogFoldChangeTable.pl
 with open(input_dir+'genotypes_BIOS_LLDeep_Diagnostics_merged_phasing_noRnaEditing.logFoldChange.depthFiltere.BINOM.samplesFILTERED.txt') as input_file:
+    print('read second logFC file')
     header = input_file.readline().strip().split('\t')
     for line in input_file:
         line = line.strip().split('\t')
@@ -74,99 +77,109 @@ count_per_inheritance['other'] = {'outlier':{}, 'not_outlier':{}, 'na':{}}
 s = set([])
 s2 = set([])
 
-with open('/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/omim_carriers/OMIM_carriers_allIMPACT.txt') as input_file:
-    input_file.readline()
-    x = 0
-    for line in input_file:
-        x += 1
-        if x % 100 == 0:
-            print(x,'lines processed')
-        line = line.strip().split('\t')
-        gene, sample, snp, genotype, symbol, impact, isOmim, MAF = line[0],line[2],line[1],line[3],line[4],line[7],line[8], line[6]
-        s2.add(gene)
-        if gene not in outliers and gene not in not_outliers:
-             s.add(gene)
-             continue
-        if line[2] in outliers[gene]:
-            if symbol in manifestation:
-                for manifestationName in manifestation[symbol]:
-                    if impact not in count_per_manifestation[manifestationName]['outlier']:
-                        count_per_manifestation[manifestationName]['outlier'][impact] = []
-                    count_per_manifestation[manifestationName]['outlier'][impact].append([gene, snp, sample, MAF])
-                for inheritanceName in inheritance[symbol]:
-                    if impact not in count_per_inheritance[inheritanceName]['outlier']:
-                        count_per_inheritance[inheritanceName]['outlier'][impact] = []
-                    count_per_inheritance[inheritanceName]['outlier'][impact].append([gene, snp, sample, MAF])
-            else:
-                if impact not in count_per_manifestation['other']['outlier']:
-                    count_per_manifestation['other']['outlier'][impact] = []
-                    count_per_inheritance['other']['outlier'][impact] = []
-                count_per_manifestation['other']['outlier'][impact].append([gene, snp, sample, MAF])
-                count_per_inheritance['other']['outlier'][impact].append([gene, snp, sample, MAF])
-        elif line[2] in not_outliers[gene]:
-            if symbol in manifestation:
-                for manifestationName in manifestation[symbol]:
-                    if impact not in count_per_manifestation[manifestationName]['not_outlier']:
-                        count_per_manifestation[manifestationName]['not_outlier'][impact] = []
-                    count_per_manifestation[manifestationName]['not_outlier'][impact].append([gene, snp, sample, MAF])
-                for inheritanceName in inheritance[symbol]:
-                    if impact not in count_per_inheritance[inheritanceName]['not_outlier']:
-                        count_per_inheritance[inheritanceName]['not_outlier'][impact] = []
-                    count_per_inheritance[inheritanceName]['not_outlier'][impact].append([gene, snp, sample, MAF])
-            else:
-                if impact not in count_per_manifestation['other']['not_outlier']:
-                    count_per_manifestation['other']['not_outlier'][impact] = []
-                    count_per_inheritance['other']['not_outlier'][impact] = []
-                count_per_manifestation['other']['not_outlier'][impact].append([gene, snp, sample, MAF])
-                count_per_inheritance['other']['not_outlier'][impact].append([gene, snp, sample, MAF])
-        elif line[2] in na[gene]:
-            if symbol in manifestation:
-                for manifestationName in manifestation[symbol]:
-                    if impact not in count_per_manifestation[manifestationName]['na']:
-                        count_per_manifestation[manifestationName]['na'][impact] = []
-                    count_per_manifestation[manifestationName]['na'][impact].append([gene, snp, sample, MAF])
-                for inheritanceName in inheritance[symbol]:
-                    if impact not in count_per_inheritance[inheritanceName]['na']:
-                         count_per_inheritance[inheritanceName]['na'][impact] = []
-                    count_per_inheritance[inheritanceName]['na'][impact].append([gene, snp, sample, MAF])
-            else:
-                if impact not in count_per_manifestation['other']['na']:
-                    count_per_manifestation['other']['na'][impact] = []
-                    count_per_inheritance['other']['na'][impact] = []
-                count_per_manifestation['other']['na'][impact].append([gene, snp, sample, MAF])
-                count_per_inheritance['other']['na'][impact].append([gene, snp, sample, MAF])
+# Input files made by OMIM_enrichment_hetsOnly.py
+input_files = ['/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/omim_carriers/OMIM_carriers_allIMPACT.hetsOnly.txt',
+               '/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/omim_carriers/OMIM_carriers_allIMPACT.txt']
+for f in input_files:
+    with open(f) as input_file:
+        print('processing',f)
+        input_file.readline()
+        x = 0
+        for line in input_file:
+            x += 1
+            if x % 100 == 0:
+                print(x,'lines processed')
+            line = line.strip().split('\t')
+            gene, sample, snp, genotype, symbol, impact, isOmim, MAF = line[0],line[2],line[1],line[3],line[4],line[7],line[8], line[6]
+            s2.add(gene)
+            if gene not in outliers and gene not in not_outliers:
+                 s.add(gene)
+                 continue
+            if line[2] in outliers[gene]:
+                if symbol in manifestation:
+                    for manifestationName in manifestation[symbol]:
+                        if impact not in count_per_manifestation[manifestationName]['outlier']:
+                            count_per_manifestation[manifestationName]['outlier'][impact] = []
+                        count_per_manifestation[manifestationName]['outlier'][impact].append([gene, snp, sample, MAF])
+                    for inheritanceName in inheritance[symbol]:
+                        if impact not in count_per_inheritance[inheritanceName]['outlier']:
+                            count_per_inheritance[inheritanceName]['outlier'][impact] = []
+                        count_per_inheritance[inheritanceName]['outlier'][impact].append([gene, snp, sample, MAF])
+                else:
+                    if impact not in count_per_manifestation['other']['outlier']:
+                        count_per_manifestation['other']['outlier'][impact] = []
+                        count_per_inheritance['other']['outlier'][impact] = []
+                    count_per_manifestation['other']['outlier'][impact].append([gene, snp, sample, MAF])
+                    count_per_inheritance['other']['outlier'][impact].append([gene, snp, sample, MAF])
+            elif line[2] in not_outliers[gene]:
+                if symbol in manifestation:
+                    for manifestationName in manifestation[symbol]:
+                        if impact not in count_per_manifestation[manifestationName]['not_outlier']:
+                            count_per_manifestation[manifestationName]['not_outlier'][impact] = []
+                        count_per_manifestation[manifestationName]['not_outlier'][impact].append([gene, snp, sample, MAF])
+                    for inheritanceName in inheritance[symbol]:
+                        if impact not in count_per_inheritance[inheritanceName]['not_outlier']:
+                            count_per_inheritance[inheritanceName]['not_outlier'][impact] = []
+                        count_per_inheritance[inheritanceName]['not_outlier'][impact].append([gene, snp, sample, MAF])
+                else:
+                    if impact not in count_per_manifestation['other']['not_outlier']:
+                        count_per_manifestation['other']['not_outlier'][impact] = []
+                        count_per_inheritance['other']['not_outlier'][impact] = []
+                    count_per_manifestation['other']['not_outlier'][impact].append([gene, snp, sample, MAF])
+                    count_per_inheritance['other']['not_outlier'][impact].append([gene, snp, sample, MAF])
+            elif line[2] in na[gene]:
+                if symbol in manifestation:
+                    for manifestationName in manifestation[symbol]:
+                        if impact not in count_per_manifestation[manifestationName]['na']:
+                            count_per_manifestation[manifestationName]['na'][impact] = []
+                        count_per_manifestation[manifestationName]['na'][impact].append([gene, snp, sample, MAF])
+                    for inheritanceName in inheritance[symbol]:
+                        if impact not in count_per_inheritance[inheritanceName]['na']:
+                             count_per_inheritance[inheritanceName]['na'][impact] = []
+                        count_per_inheritance[inheritanceName]['na'][impact].append([gene, snp, sample, MAF])
+                else:
+                    if impact not in count_per_manifestation['other']['na']:
+                        count_per_manifestation['other']['na'][impact] = []
+                        count_per_inheritance['other']['na'][impact] = []
+                    count_per_manifestation['other']['na'][impact].append([gene, snp, sample, MAF])
+                    count_per_inheritance['other']['na'][impact].append([gene, snp, sample, MAF])
+    
+    if f.endswith('hetsOnly.txt'):
+        disease_outfile = '/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/carriers_per_disease/carriers_per_disease.hetsOnly.txt'
+        inheritance_outfile = '/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/carriers_per_inheritance/carriers_per_inheritance.hetsOnly.txt'
+    else:
+        disease_outfile = '/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/carriers_per_disease/carriers_per_disease.txt'
+        inheritance_outfile = '/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/carriers_per_inheritance/carriers_per_inheritance.txt'
+    
 
-
-disease_outfile = '/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/carriers_per_disease/carriers_per_disease.txt'
-with open(disease_outfile,'w') as out:
-    out.write('disease\ttype\timpact\tgene\tsnp\tsample\tlogFC\tMAF\n')
-    for m in count_per_manifestation:
-        for type in ['outlier','not_outlier','na']:
-            for impact in count_per_manifestation[m][type]:
-                for result in count_per_manifestation[m][type][impact]:
-                    if result[2] in logFC_per_sample_per_gene:
-                        if result[0] in logFC_per_sample_per_gene[result[2]]:
-                            out.write(m+'\t'+type+'\t'+impact+'\t'+result[0]+'\t')
-                            out.write(result[1]+'\t'+result[2]+'\t')
-                            out.write(logFC_per_sample_per_gene[result[2]][result[0]])
-                            out.write('\t'+result[3]+'\n')
+    with open(disease_outfile,'w') as out:
+        out.write('disease\ttype\timpact\tgene\tsnp\tsample\tlogFC\tMAF\n')
+        for m in count_per_manifestation:
+            for type in ['outlier','not_outlier','na']:
+                for impact in count_per_manifestation[m][type]:
+                    for result in count_per_manifestation[m][type][impact]:
+                        if result[2] in logFC_per_sample_per_gene:
+                            if result[0] in logFC_per_sample_per_gene[result[2]]:
+                                out.write(m+'\t'+type+'\t'+impact+'\t'+result[0]+'\t')
+                                out.write(result[1]+'\t'+result[2]+'\t')
+                                out.write(logFC_per_sample_per_gene[result[2]][result[0]])
+                                out.write('\t'+result[3]+'\n')
+                            else:
+                                print(result[0],'not in logFC_per_sample_per_gene[result[2]')
                         else:
-                            print(result[0],'not in logFC_per_sample_per_gene[result[2]')
-                    else:
-                        print(result[2],'not in logFC_per_sample_per_gene')
+                            print(result[2],'not in logFC_per_sample_per_gene')
 
-inheritance_outfile = '/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/carriers_per_inheritance/carriers_per_inheritance.txt'
-with open(inheritance_outfile,'w') as out:
-    out.write('inheritance\ttype\timpact\tgene\tsnp\tsample\tlogFC\tMAF\n')
-    for m in count_per_inheritance:
-        for type in ['outlier','not_outlier','na']:
-            for impact in count_per_inheritance[m][type]:
-                for result in count_per_inheritance[m][type][impact]:
-                    if result[2] in logFC_per_sample_per_gene:
-                        if result[0] in logFC_per_sample_per_gene[result[2]]:
-                            out.write(m+'\t'+type+'\t'+impact+'\t'+result[0]+'\t')
-                            out.write(result[1]+'\t'+result[2]+'\t')
-                            out.write(logFC_per_sample_per_gene[result[2]][result[0]])
-                            out.write('\t'+result[3]+'\n')
+    with open(inheritance_outfile,'w') as out:
+        out.write('inheritance\ttype\timpact\tgene\tsnp\tsample\tlogFC\tMAF\n')
+        for m in count_per_inheritance:
+            for type in ['outlier','not_outlier','na']:
+                for impact in count_per_inheritance[m][type]:
+                    for result in count_per_inheritance[m][type][impact]:
+                        if result[2] in logFC_per_sample_per_gene:
+                            if result[0] in logFC_per_sample_per_gene[result[2]]:
+                                out.write(m+'\t'+type+'\t'+impact+'\t'+result[0]+'\t')
+                                out.write(result[1]+'\t'+result[2]+'\t')
+                                out.write(logFC_per_sample_per_gene[result[2]][result[0]])
+                                out.write('\t'+result[3]+'\n')
 
-print('output written to',disease_outfile,'and',inheritance_outfile)
+    print('output written to',disease_outfile,'and',inheritance_outfile)
