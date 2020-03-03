b687e47aa39eaf658b6f78ebd1a47ea703b102a6
npklein <niekdeklein@gmail.com>
2020-03-03 15:43:56 +0100

    refractor for clarity

---
+++ figure_2_and_3/get_omim_genes.py
@@ -0,0 +1,13 @@
+dir='/groups/umcg-bios/tmp03/projects/outlierGeneASE/geneAndVariantLists/'
+with open(dir+'OMIM_20180211.mim2gene.txt') as input_file, open(dir+'OMIM.20171220.ensembleGenes.txt','w') as out:
+    for line in input_file:
+        line = line.strip()
+        gene = line.split('\t')[-1]
+        try:
+            symbol = line.split('\t')[-2]
+            if len(symbol.strip()) == 0:
+                symbol = 'NA'
+        except:
+            symbol = 'NA'
+        if gene.startswith('ENSG'):
+            out.write(gene+'\t'+symbol+'\n')
