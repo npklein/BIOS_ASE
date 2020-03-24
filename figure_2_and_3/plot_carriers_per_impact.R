88b8fb7355cb4d3376c394a45b139c2ed568b929
npklein <niekdeklein@gmail.com>
2020-03-24 18:13:38 +0100

    remove overlap with vkgl etc

--- figure_2_and_3/plot_carriers_per_impact.R
+++ figure_2_and_3/plot_carriers_per_impact.R
@@ -6,6 +6,7 @@ library(plotly)
 require(dplyr)
 
 carriers_per_disease <- fread('/groups/umcg-bios/tmp03/projects/outlierGeneASE/omim_enrichment/carriers_per_disease/carriers_per_disease.txt')
+#carriers_per_disease <- fread('carriers_per_disease.txt')
 
 
 carriers_per_disease <- carriers_per_disease[!is.na(carriers_per_disease$logFC),]
@@ -15,17 +16,19 @@ give.n <- function(x){
   return(c(y = -0.1, label = length(x)))
 }
 
-carriers_per_disease_snpInfo[carriers_per_disease_snpInfo$disease=='other',]$disease <- 'General'
-carriers_per_disease_snpInfo <- carriers_per_disease_snpInfo[!carriers_per_disease_snpInfo$disease=='General',]
-outliers_per_disease <- carriers_per_disease_snpInfo %>%
-  group_by(disease,CLNVRSIG) %>%
+carriers_per_disease_high[carriers_per_disease_high$disease=='other',]$disease <- 'General'
+carriers_per_disease_high <- carriers_per_disease_high[!carriers_per_disease_high$disease=='General',]
+
+outliers_per_disease <- carriers_per_disease %>%
+  group_by(disease,impact) %>%
    summarise(outlier = sum(type=='outlier'),
              not_outlier = sum(type=='not_outlier'),
              n=n())
 
 outliers_per_disease$fraction_outlier <- outliers_per_disease$outlier/(outliers_per_disease$outlier+outliers_per_disease$not_outlier)
+outliers_per_disease_noModifier <- outliers_per_disease[outliers_per_disease$impact!="MODIFIER",]
 
-ggplot(outliers_per_disease, aes(CLNVRSIG, fraction_outlier, fill=CLNVRSIG))+
+ggplot(outliers_per_disease_noModifier, aes(impact, fraction_outlier, fill=impact))+
   geom_bar(stat='identity')+
   theme_bw(base_size = 18)+
   facet_wrap(~disease, nrow=3)+
@@ -33,9 +36,7 @@ ggplot(outliers_per_disease, aes(CLNVRSIG, fraction_outlier, fill=CLNVRSIG))+
   ylab('Fraction of SNPs that is an outlier')+
   xlab('')+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  geom_text(aes(label=n), vjust=-1)+
-  scale_x_discrete(limit=c('Pathogenic',
-                           'Uncertain_significance','Benign'))+
+  #geom_text(aes(label=n), vjust=-1)+
   scale_y_continuous(limit=c(0,1.1))
 
 outfile = '/groups/umcg-bios/tmp03/projects/BIOS_manuscript/fig3//proportion_outlier_per_clinvar_per_disease.png'
