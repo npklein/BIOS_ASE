95236ad6e52a86858ceac3313d0fa82e1a4753fe
npklein <niekdeklein@gamail.com>
2021-03-27 16:26:38 +0100

    add before pull

--- figure_2_and_3/plot_carriers_per_impact.R
+++ figure_2_and_3/plot_carriers_per_impact.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(plotly)
 require(dplyr)
 
-carriers_per_disease <- fread('/groups/umcg-bios/tmp04/projects/copy_from_tmp03/outlierGeneASE/omim_enrichment/carriers_per_disease/carriers_per_disease.txt')
+carriers_per_disease <- fread('/groups/umcg-bios/tmp04/projects/copy_from_tmp03/outlierGeneASE/omim_enrichment/carriers_per_disease/carriers_per_disease.txt.gz')
 #carriers_per_disease <- fread('carriers_per_disease.txt')
 
 
